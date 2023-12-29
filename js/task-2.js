a78af3fc25e0efade42f62cdf1fd37890268d99a
Darialucky <144655337+Darialucky@users.noreply.github.com>
2023-12-29 15:09:13 +0200

    happy

--- js/task-2.js
+++ js/task-2.js
@@ -1,62 +1,25 @@
-function makeArray (firstArray,secondArray,maxLength ) {
-const combiArray = [].concat(firstArray,secondArray);
-const result = combiArray.slice(0,maxLength);
-  if ( combiArray.length>=maxLength) {
-    return result;
+class Storage {
+  #items;
+
+  constructor(items) {
+    this.#items = items;
   }
-  else {
-    return combiArray ;
+
+  getItems() {
+    return this.#items;
   }
-}
 
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*function getShippingMessage(country, price, deliveryFee) {
-  let totalPrice = price + deliveryFee;
-  const messagePrice = `Shipping to ${country} will cost ${totalPrice} credits`;
-  return messagePrice;
-}
-
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
-
-
-function formatMessage(message, maxLength) {
-  if (message.length <= maxLength) {
-    return message;
+  addItem(newItem) {
+    this.#items.push(newItem);
   }
-  else {return message.slice(0, maxLength) + '...';
 
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter(item => item !== itemToRemove); ;
   }
 }
-console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
-console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
-console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
-console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"*/
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
